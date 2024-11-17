40340222ec79d4ea2b34b9290b2d5fe1f0bd9434
sultan-uniy <ssuultan135@gmail.com>
2024-11-17 21:36:55 +0300

    Update ListTester.java

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -3,7 +3,17 @@ package com.example.project;
 public class ListTester {
     public static <T> void circularLeftShift(List <T> list, int n)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (list.empty()) return;
+    for (int i = 0; i < n; i++) {
+        list.findFirst();
+        T firstElement = list.retrieve();
+        list.remove();
+        list.findFirst();
+        while (!list.last()) {
+            list.findNext();
+        }
+        list.insert(firstElement);
+    }
         // Write the method circularLeftShift, user of List ADT, that takes as input a non-empty
         // List list and an integer n > 0 and performs n circular left shift of the list.
         // Example 2.1. Given the list l : A, B, C, D, E, circularShiftLeft(l, 1) results in
@@ -11,7 +21,16 @@ public class ListTester {
     }
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (l1.empty()) return;
+    l1.findFirst();
+    while (!l1.last()) {
+        l1.findNext();
+    }
+    while (true) {
+        l2.insert(l1.retrieve());
+        if (l1.first()) break;
+        l1.findPrevious();
+    }
         // Write the method reverseCopy, user of DoubleLinkedList, which copies the elements
         // of l1 to l2 in reverse order. The list l1 must not change. Assume that l2 is empty.
         // The method signature is public static <T> void reverseCopy(DoubleLinkedList<T> l1,
